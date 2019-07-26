69aacc5e6f69e16a84adc4b987e31998c48241c8
isjaki <usow.aleksey@yandex.ru>
2019-07-27 01:00:14 +0300

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Header from './components/Header/Header';
 import AdvertsList from './components/AdvertsList/AdvertsList';
 import FiltersWidget from './components/FiltersWidget/FiltersWidget';
 import styles from './App.css';
 
 const App = () => (
     <div className={styles.app}>
+        <Header />
         <AdvertsList />
         <FiltersWidget />
     </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Header.css';
+
+const Header = () => (
+    <header className={styles.header}>
+        Найдено объявлений: 1
+    </header>
+);
+
+export default Header;
