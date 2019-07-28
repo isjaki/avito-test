88277573d32032964ecf4d7fb687335ac66ef0ce
isjaki <usow.aleksey@yandex.ru>
2019-07-28 14:01:48 +0300

    modified the PriceFilter component: created an input handler function

--- src/components/UI/PriceFilter/PriceFilter.js
+++ src/components/UI/PriceFilter/PriceFilter.js
@@ -7,12 +7,37 @@ class PriceFilter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            priceFrom: 0,
-            priceTo: Infinity,
+            priceFrom: '',
+            priceTo: '',
         };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.forwardInputValues = this.forwardInputValues.bind(this);
+    }
+
+    handleInputChange(event) {
+        const inputValue = event.target.value;
+
+        if (!/^[0-9]*\.?[0-9]*$/.test(inputValue)) return;
+
+        this.setState({ [event.target.id]: inputValue }, () => {
+            this.forwardInputValues();
+        });
+    }
+
+    forwardInputValues() {
+        const { onPriceChange } = this.props;
+        let { priceFrom, priceTo } = this.state;
+
+        priceFrom = parseFloat(priceFrom) || 0;
+        priceTo = parseFloat(priceTo) || Infinity;
+
+        onPriceChange(priceFrom, priceTo);
     }
 
     render() {
+        const { priceFrom, priceTo } = this.state;
+
         return (
             <div className={styles.priceFilter}>
                 <label htmlFor="priceFrom" className={styles.priceLabel}>
@@ -20,7 +45,9 @@ class PriceFilter extends Component {
                     <input
                         type="text"
                         id="priceFrom"
+                        value={priceFrom}
                         className={styles.priceInput}
+                        onChange={this.handleInputChange}
                     />
                 </label>
                 <label htmlFor="priceTo" className={styles.priceLabel}>
@@ -28,7 +55,9 @@ class PriceFilter extends Component {
                     <input
                         type="text"
                         id="priceTo"
+                        value={priceTo}
                         className={styles.priceInput}
+                        onChange={this.handleInputChange}
                     />
                 </label>
             </div>
@@ -37,7 +66,7 @@ class PriceFilter extends Component {
 }
 
 PriceFilter.propTypes = {
-
+    onPriceChange: PropTypes.func.isRequired,
 };
 
 export default PriceFilter;
