5fc72c775c91aff6d78529f56e33cf11fb22f433
isjaki <usow.aleksey@yandex.ru>
2019-07-30 00:09:19 +0300

    added styles to the Advert component, created RatingWidget component

---
+++ src/components/UI/RatingWidget/RatingWidget.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import styles from './RatingWidget.css';
+
+const RatingWidget = ({ rating }) => {
+    const classes = classNames({
+        [styles.ratingWidget]: true,
+        [styles.high]: rating >= 4,
+        [styles.medium]: rating >= 3 && rating < 4,
+        [styles.low]: rating < 3,
+    });
+
+    return (
+        <span className={classes}>
+            <i className="fas fa-star" /> {rating}
+        </span>
+    );
+};
+
+RatingWidget.propTypes = {
+    rating: PropTypes.number.isRequired,
+};
+
+export default RatingWidget;
