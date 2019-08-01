6c273634268178c347fc925b5aeb599af814f325
isjaki <usow.aleksey@yandex.ru>
2019-08-01 22:27:45 +0300

    changed the Filters container, added "debounce" helper function

--- src/containers/Filters/Filters.js
+++ src/containers/Filters/Filters.js
@@ -1,30 +1,66 @@
 import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { debounce } from '../../helpers/helpers';
+
+import * as filterActions from '../../store/actions/filters';
 
 import FormElementContainer from '../../components/UI/FormElementContainer/FormElementContainer';
 import Selector from '../../components/UI/Selector/Selector';
 import PriceFilter from '../../components/UI/PriceFilter/PriceFilter';
 import Button from '../../components/UI/Button/Button';
 
-const Filters = () => (
+const selectorOptions = [
+    { value: 'all', displayValue: 'Все' },
+    { value: 'immovable', displayValue: 'Недвижимость' },
+    { value: 'cameras', displayValue: 'Фотоаппараты' },
+    { value: 'auto', displayValue: 'Автомобили' },
+    { value: 'laptops', displayValue: 'Ноутбуки' },
+];
+
+const Filters = ({
+    setCategoryFilter,
+    setPriceFilter,
+    setFavoritesOnlyFilter,
+    isFavoritesOnly,
+}) => (
     <Fragment>
         <FormElementContainer label="По категории:">
             <Selector
-                options={[
-                    { value: 'all', displayValue: 'Все' },
-                    { value: 'immovable', displayValue: 'Недвижимость' },
-                    { value: 'cameras', displayValue: 'Фотоаппараты' },
-                    { value: 'auto', displayValue: 'Автомобили' },
-                    { value: 'laptops', displayValue: 'Ноутбуки' },
-                ]}
+                options={selectorOptions}
+                initialValue="all"
+                onSelectorChange={setCategoryFilter}
             />
         </FormElementContainer>
         <FormElementContainer label="По цене:">
-            <PriceFilter />
+            <PriceFilter onPriceChange={debounce(setPriceFilter, 800)} />
         </FormElementContainer>
         <FormElementContainer>
-            <Button buttonType="pickFavorites" />
+            <Button
+                buttonType="pickFavorites"
+                onClick={setFavoritesOnlyFilter}
+                isActive={isFavoritesOnly}
+            />
         </FormElementContainer>
     </Fragment>
 );
 
-export default Filters;
+Filters.propTypes = {
+    setCategoryFilter: PropTypes.func.isRequired,
+    setPriceFilter: PropTypes.func.isRequired,
+    setFavoritesOnlyFilter: PropTypes.func.isRequired,
+    isFavoritesOnly: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = state => ({
+    isFavoritesOnly: state.filters.isFavoritesOnly,
+});
+
+const mapDispatchToProps = dispatch => ({
+    setCategoryFilter: categoryType => dispatch(filterActions.setCategoryFilter(categoryType)),
+    setPriceFilter: (priceFrom, priceTo) => dispatch(filterActions.setPriceFilter(priceFrom, priceTo)),
+    setFavoritesOnlyFilter: () => dispatch(filterActions.setFavoritesOnlyFilter()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filters);
