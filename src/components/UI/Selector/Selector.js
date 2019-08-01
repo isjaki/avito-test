20276fda8ec496cb9992c8bb2658c85bdae27db0
isjaki <usow.aleksey@yandex.ru>
2019-08-01 21:25:56 +0300

    changed the Selector component: turned it into a class and added state with "value" property

--- src/components/UI/Selector/Selector.js
+++ src/components/UI/Selector/Selector.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Selector.css';
 
-const Selector = ({ options, value }) => (
-    <select value={value} className={styles.selector}>
-        {options.map(option => (
-            <option key={option.value} value={option.value}>{option.displayValue}</option>
-        ))}
-    </select>
-);
+class Selector extends Component {
+    constructor(props) {
+        super(props);
+        const { initialValue } = this.props;
+
+        this.state = {
+            value: initialValue,
+        };
+
+        this.handleSelectorChange = this.handleSelectorChange.bind(this);
+    }
+
+    handleSelectorChange(event) {
+        const { onSelectorChange } = this.props;
+        const { value } = this.state;
+        this.setState({ value: event.target.value }, () => {
+            onSelectorChange(value);
+        });
+    }
+
+    render() {
+        const { options } = this.props;
+        const { value } = this.state;
+
+        return (
+            <select value={value} className={styles.selector} onChange={this.handleSelectorChange}>
+                {options.map(option => (
+                    <option key={option.value} value={option.value}>{option.displayValue}</option>
+                ))}
+            </select>
+        );
+    }
+}
 
 Selector.propTypes = {
     options: PropTypes.arrayOf(PropTypes.object).isRequired,
-    value: PropTypes.string.isRequired,
+    initialValue: PropTypes.string.isRequired,
+    onSelectorChange: PropTypes.func.isRequired,
 };
 
 export default Selector;
