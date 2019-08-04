a377a7821b3054b19916d0aeecaf4cc79cf22830
isjaki <usow.aleksey@yandex.ru>
2019-08-05 01:18:08 +0300

    implemented image lazy loading with the help of react-lazy-load package

--- src/components/Product/ProductPicture/ProductPicture.js
+++ src/components/Product/ProductPicture/ProductPicture.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LazyLoad from 'react-lazy-load';
 
 import styles from './ProductPicture.css';
 
 const ProductPicture = ({ pictures }) => (
     <div className={styles.productPicture}>
-        <img
-            alt="product"
-            src={pictures[0]}
-        />
+        <LazyLoad offsetVertical={900}>
+            <img alt="product" src={pictures[0]} />
+        </LazyLoad>
         <div className={styles.additionalPicturesNumber}><span>+{pictures.length - 1}</span></div>
     </div>
 );
